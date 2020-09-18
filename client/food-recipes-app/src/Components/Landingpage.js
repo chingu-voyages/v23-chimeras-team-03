8501254917e4fd7ae3239585607e869a4d756807
Rayhan1998 <miahrayhan01@gmail.com>
2020-09-18 18:34:44 -0400

    added a header to landing page

---
+++ client/food-recipes-app/src/Components/Landingpage.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "../App.css";
+function Landingpage() {
+  const [search, setSearch] = useState("");
+
+  function searchHandler(e) {
+    setSearch(e.target.value);
+  }
+
+  function formSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <nav className="navbar">
+      <h1>Logo</h1>
+      <form onSubmit={formSubmit}>
+        <input onChange={searchHandler} value={search} type="text"></input>
+        <button>Search</button>
+      </form>
+      <form>
+        <button>Sign Up</button>
+        <button>login</button>
+      </form>
+    </nav>
+  );
+}
+
+export default Landingpage;

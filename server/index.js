96382bf15c581aabad7cb8fcc7f58a2c68fc8d29
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-09-24 21:40:25 -0400

    sets up backend

--- server/index.js
+++ server/index.js
@@ -1,36 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-import sequelize from "./utils/sequelize.js";
-import syncModels from "./utils/syncModels.js";
+const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 7000;
 
-const main = async () => {
-  const app = express();
-  dotenv.config({ path: "./config" });
+const app = express();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+// }
 
-  //load .env variable
+app.use(cors());
+app.use(express.json());
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  // middleware to parse json & urlencoded data
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-
-  // testing connection
-
-  await syncModels(sequelize);
-
-  const PORT = 8080;
-
-  app.listen(PORT, () => {
-    console.log(`app is listening on http://localhost:${PORT}`);
-  });
-  //listen for requests on http://localhost:8080
-};
-
-main();
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}`);
+});

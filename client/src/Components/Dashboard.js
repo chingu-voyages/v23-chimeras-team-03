875e108482ec0dc4e5b46548cde2280a41523ea3
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-09-26 22:32:56 -0400

    sets up sign in and logout function on dashboard

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const Dashboard = () => {
+const Dashboard = ({ setAuth }) => {
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+    toast.success("Logged out successfully");
+  };
   return (
     <div>
       <h1>Dashboard</h1>
+      <button onClick={(e) => logout(e)}>Log out</button>
     </div>
   );
 };

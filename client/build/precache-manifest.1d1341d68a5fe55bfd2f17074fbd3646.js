f9c0eafa5885b292a28f55b2bc356faa8ff4ee28
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-10-04 10:27:30 -0400

    comment out build in gitignore

---
+++ client/build/precache-manifest.1d1341d68a5fe55bfd2f17074fbd3646.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291e6512ede59ccc773c8a47b19be969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "accfba54e314d7f54090",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6e4d4e140b36785bf12c",
+    "url": "/static/css/main.df421d55.chunk.css"
+  },
+  {
+    "revision": "accfba54e314d7f54090",
+    "url": "/static/js/2.ca875aff.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.ca875aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4d4e140b36785bf12c",
+    "url": "/static/js/main.8e493c6d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c6c5564bac6d775cb4760d99ed27d247",
+    "url": "/static/media/foody.c6c5564b.jpg"
+  }
+]);
